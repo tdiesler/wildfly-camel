e8b582320639a08d90181d92a4961bdd2f88b3a7
Thomas Diesler <thomas.diesler@jboss.com>
2015-04-13 15:54:52 +0200

    [resolves #530] Remove dependency on Felix SCR and Gravia Provisioner

--- enricher/src/main/java/org/wildfly/camel/arquillian/CamelRemoteLoadableExtension.java
+++ enricher/src/main/java/org/wildfly/camel/arquillian/CamelRemoteLoadableExtension.java
@@ -7,9 +7,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -34,6 +34,5 @@ public class CamelRemoteLoadableExtension implements RemoteLoadableExtension {
     public void register(ExtensionBuilder builder) {
         builder.service(ResourceProvider.class, CamelContextFactoryProvider.class);
         builder.service(ResourceProvider.class, CamelContextRegistryProvider.class);
-        builder.service(ResourceProvider.class, ProvisionerProvider.class);
     }
 }
