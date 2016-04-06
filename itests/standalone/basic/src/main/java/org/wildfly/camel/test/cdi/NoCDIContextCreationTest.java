f1bf433406e820a8f7f5cdb2478f25fb6aaa681f
Thomas Diesler <thomas.diesler@jboss.com>
2016-04-06 11:22:49 +0200

    [resolves #1179] Move CDI proxy filtering to upstream camel

---
+++ itests/standalone/basic/src/main/java/org/wildfly/camel/test/cdi/NoCDIContextCreationTest.java
@@ -0,0 +1,59 @@
+/*
+ * #%L
+ * Wildfly Camel :: Testsuite
+ * %%
+ * Copyright (C) 2013 - 2016 RedHat
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
+package org.wildfly.camel.test.cdi;
+
+import java.util.Set;
+
+import org.apache.camel.CamelContext;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.arquillian.test.api.ArquillianResource;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.EmptyAsset;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.wildfly.camel.test.common.types.HelloBean;
+import org.wildfly.extension.camel.CamelAware;
+import org.wildfly.extension.camel.CamelContextRegistry;
+
+@CamelAware
+@RunWith(Arquillian.class)
+public class NoCDIContextCreationTest {
+
+    @ArquillianResource
+    CamelContextRegistry contextRegistry;
+
+    @Deployment
+    public static JavaArchive deployment() {
+        final JavaArchive archive = ShrinkWrap.create(JavaArchive.class, "no-camel-cdi-test.jar");
+        archive.addClasses(HelloBean.class);
+        archive.addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml");
+        return archive;
+    }
+
+    @Test
+    public void testDeployment() throws Exception {
+        Set<CamelContext> contexts = contextRegistry.getCamelContexts();
+        Assert.assertTrue("No contexts deployed", contexts.isEmpty());
+    }
+}
