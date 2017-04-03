3a84b53c3bac3f12c57819dcfd180ec377a32872
Thomas Diesler <thomas.diesler@jboss.com>
2017-04-03 09:07:17 +0200

    Ignore [ENTESB-6694] Camel LinkedIn not compatible with EAP provided JAX-RS API

--- itests/standalone/extra/src/main/java/org/wildfly/camel/test/linkedin/LinkedInIntegrationTest.java
+++ itests/standalone/extra/src/main/java/org/wildfly/camel/test/linkedin/LinkedInIntegrationTest.java
@@ -18,12 +18,14 @@ import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.spec.JavaArchive;
 import org.junit.Assert;
 import org.junit.Assume;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.wildfly.extension.camel.CamelAware;
 
 @CamelAware
 @RunWith(Arquillian.class)
+@Ignore("[ENTESB-6694] Camel LinkedIn not compatible with EAP provided JAX-RS API")
 public class LinkedInIntegrationTest {
 
     // Enum values correspond to environment variable names
